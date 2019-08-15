116c875e9e6651e1b62dbbc2bcb4fe6687c73c7c
Valkyrie Hazelnut <vhazelnu@at-n3.21-school.ru>
2019-08-15 16:20:55 +0300

    validation

--- validation.c
+++ validation.c
@@ -1,14 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lem_in.h                                           :+:      :+:    :+:   */
+/*   validation.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vhazelnu <vhazelnu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/15 16:12:44 by vhazelnu          #+#    #+#             */
-/*   Updated: 2019/08/15 16:14:44 by vhazelnu         ###   ########.fr       */
+/*   Created: 2019/08/15 16:04:51 by vhazelnu          #+#    #+#             */
+/*   Updated: 2019/08/15 16:18:10 by vhazelnu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "lem_in.h"
+#include <fcntl.h>
 
-#ifndef LEM_IN_H
+int		validation(char *str)
+{
+	int		fd;
+	char	*line;
+
+	fd = open(str, O_RDONLY);
+	while (get_next_line(fd, &line) == 1)
+	{
+		
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		if (!validation(argv[1]))
+		{
+			write(2, "ERROR\n", 6);
+			return (0);
+		}
+	}
+	return (0);
+}
